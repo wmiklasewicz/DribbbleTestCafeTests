b0241676dae14c5b4baafaef6bb9fbd59bdb4253
Wioletta Kisielewicz <wioletta.kisielewicz@intive.com>
2020-01-19 02:35:12 +0100

    added edit account tests

--- tests/fixtures/editAccount.js
+++ tests/fixtures/editAccount.js
@@ -9,7 +9,6 @@ fixture('Edit personal profile')
    await accountPage.goto();
 });
 
-/*
 test('should edit user name and location', async t => {
    let randomName = faker.name.findName().toString();
    let randomLocation = faker.address.city().toString();
@@ -22,9 +21,7 @@ test('should edit user name and location', async t => {
    await t.expect((accountPage.locationInput).value).eql(randomLocation);
    await t.expect((accountPage.successMsg).exists).ok();
 });
-*/
 
-/*
 test('should edit bio', async t => {
    let randomBio = faker.lorem.paragraph().toString();
    accountPage.clearInput(accountPage.bioInput);
@@ -32,31 +29,30 @@ test('should edit bio', async t => {
    await t.click(accountPage.saveProfileButton);
    await t.expect((accountPage.successMsg).exists).ok();
 });
-*/
 
-
-
-/*test('should select top specialties and add years of experience', async t => {
-   let label  =  accountPage.ilustrationSpecialities;
-   if (label.checked == true){
-      await t.click(accountPage.ilustrationSpecialities);
+test('should select top specialties and add years of experience', async t => {
+   const experiencePickedAmount = await Selector(accountPage.dragableContainer).childElementCount;
+   let i = 1;
+   if (experiencePickedAmount>0)
+   {
+      for(i=1;i<=experiencePickedAmount;i++)
+      {
+         await t.click(accountPage.dragableContainerFirst);
+      }
    }
-   await t.wait(63333);
-   
-   //await t.click(accountPage.selectYearsWidget);
-   //await t.click(accountPage.experience3years);
-   //await t.click(accountPage.saveProfileButton);
-});*/
-
-/*
-test('should try to save profile without name', async t => {
-   accountPage.clearInput(accountPage.nameInput);
+   else
+{
+   for(i=1;i<=2;i++)
+   {
+      await t.click(Selector(accountPage.experiencePickConcatInactive + i + ')'));
+      await t.click(Selector(accountPage.experiencePickConcatActive + i + ')' + accountPage.experienceYearArrowConcat));
+      await t.click(Selector(accountPage.experiencePickConcatActive + i + ')' + accountPage.experienceYearPickConcat));
+   }
+}
    await t.click(accountPage.saveProfileButton);
-   await t.expect((accountPage.errorMsg).exists).ok();
-
+   await t.expect((accountPage.successMsg).exists).ok();
 });
-*/
-/*
+
 test('should edit job history', async t =>{
    await accountPage.goto();
    let randomTitle = faker.commerce.department().toString();
@@ -71,9 +67,8 @@ test('should edit job history', async t =>{
    await t.expect((accountPage.jobHistoryTitle).value).eql(randomTitle);
    await t.expect((accountPage.jobHistoryCompany).value).eql(randomCompany);
    await t.expect((accountPage.successMsg).exists).ok();
-});*/
+});
 
-/*
 test('should edit online presence values', async t =>{
    await accountPage.goto();
    let randomUrl = faker.internet.url().toString();
@@ -82,9 +77,8 @@ test('should edit online presence values', async t =>{
    await t.click(accountPage.saveProfileButton);
    await t.expect((accountPage.successMsg).exists).ok();
 });
-*/
 
-/*test('should upload and delete avatar', async t =>{
+test('should upload and delete avatar', async t =>{
    await accountPage.goto();
    await t.setFilesToUpload(accountPage.userAwatarButton,[
       '../../data/images/awatar.jpg'
@@ -95,23 +89,18 @@ test('should edit online presence values', async t =>{
    await t.setNativeDialogHandler(() => true);
    await t.click(accountPage.deleteAwatarButton);
    await t.expect(accountPage.awatarMsg.withText('Avatar deleted')).ok();
-});*/
 
-/*
+});
    test('should edit work availability', async t => {
-   
+   await t.maximizeWindow();
       let salaryProfile = accountPage.salaryProfile;
       let startingProfile = accountPage.profileStarting;
       let startingOption = startingProfile.find('option');
       let salaryOption = salaryProfile.find('option');
-      let isChecked  = await t.expect((accountPage.workAvailabilitySection).exists).ok();
-      await t.wait(1000);
-         if(isChecked == true){
-          accountPage.uncheckOption();
+      let isNotChecked  = await (Selector('#new_profile > div.d-none').exists);
+         if(isNotChecked == true){
+            await t.click(accountPage.fullTimeCheckBox);
          }
-      await t.wait(5000);
-
-   await t.click(accountPage.fullTimeCheckBox);
    await t.click(salaryProfile);
    await t.click(salaryOption.withText('50-60k'));
    await t.expect(salaryProfile.value).eql('50-60k');
@@ -120,16 +109,4 @@ test('should edit online presence values', async t =>{
    await t.expect(startingProfile.value).eql('immediately');
    await t.click(accountPage.saveProfileButton);
    await t.expect((accountPage.successMsg).exists).ok();
-   
 });
-*/
-/*
-test('should add new skill', async  t =>{
-   let randomSkill = faker.lorem.word().toString();
-   await t.click(accountPage.skillsInput);
-   await t.pressKey('delete');
-   await t.typeText(accountPage.skillsInput, randomSkill);
-   await t.click(accountPage.saveProfileButton);
-   await t.expect((accountPage.successMsg).exists).ok();
-});*/
-
