1bdfa002f53b51b8a979cda8627d46aad61cf5d0
Wioletta Kisielewicz <wioletta.kisielewicz@intive.com>
2020-01-19 01:29:23 +0100

    added job board tests

--- tests/fixtures/editAccount.js
+++ tests/fixtures/editAccount.js
@@ -1,6 +1,7 @@
 import { validUser } from '../../data/user';
 import accountPage from '../../pageObjects/accountPage';
 import faker from 'faker';
+import { Selector } from 'testcafe';
 
 fixture('Edit personal profile')
 .beforeEach(async t => {
@@ -19,7 +20,7 @@ test('should edit user name and location', async t => {
    await t.click(accountPage.saveProfileButton);
    await t.expect((accountPage.nameInput).value).eql(randomName);
    await t.expect((accountPage.locationInput).value).eql(randomLocation);
-   //await t.expect(accountPage.successMsg).eql('Profile updated successfully');
+   await t.expect((accountPage.successMsg).exists).ok();
 });
 */
 
@@ -29,25 +30,106 @@ test('should edit bio', async t => {
    accountPage.clearInput(accountPage.bioInput);
    await t.typeText(accountPage.bioInput, randomBio);
    await t.click(accountPage.saveProfileButton);
-   await t.expect((accountPage.successMsg).value).contains('Profile updated successfully')
+   await t.expect((accountPage.successMsg).exists).ok();
 });
 */
 
- /* 
-test('should select top specialties and add years of experience', async t => {
-    await t.click(accountPage.leadershipSpecialities);
-      await t.click(accountPage.selectYearsWidget);
-      await t.click(accountPage.experience3years);
-      await t.click(accountPage.saveProfileButton);
+
+
+/*test('should select top specialties and add years of experience', async t => {
+   let label  =  accountPage.ilustrationSpecialities;
+   if (label.checked == true){
+      await t.click(accountPage.ilustrationSpecialities);
+   }
+   await t.wait(63333);
+   
+   //await t.click(accountPage.selectYearsWidget);
+   //await t.click(accountPage.experience3years);
+   //await t.click(accountPage.saveProfileButton);
+});*/
+
+/*
+test('should try to save profile without name', async t => {
+   accountPage.clearInput(accountPage.nameInput);
+   await t.click(accountPage.saveProfileButton);
+   await t.expect((accountPage.errorMsg).exists).ok();
+
+});
+*/
+/*
+test('should edit job history', async t =>{
+   await accountPage.goto();
+   let randomTitle = faker.commerce.department().toString();
+   let randomCompany = faker.company.companyName().toString();
+   accountPage.clearInput(accountPage.jobHistoryTitle);
+   accountPage.clearInput(accountPage.jobHistoryCompany);
+   await t.typeText(accountPage.jobHistoryTitle, randomTitle);
+   await t.typeText(accountPage.jobHistoryCompany, randomCompany);
+   await t.click(accountPage.startMonthJobDropBox);
+   await t.click(accountPage.monthValue);
+   await t.click(accountPage.saveProfileButton);
+   await t.expect((accountPage.jobHistoryTitle).value).eql(randomTitle);
+   await t.expect((accountPage.jobHistoryCompany).value).eql(randomCompany);
+   await t.expect((accountPage.successMsg).exists).ok();
+});*/
+
+/*
+test('should edit online presence values', async t =>{
+   await accountPage.goto();
+   let randomUrl = faker.internet.url().toString();
+   accountPage.clearInput(accountPage.personalWebsite);
+   await t.typeText(accountPage.personalWebsite, randomUrl);
+   await t.click(accountPage.saveProfileButton);
+   await t.expect((accountPage.successMsg).exists).ok();
+});
+*/
+
+/*test('should upload and delete avatar', async t =>{
+   await accountPage.goto();
+   await t.setFilesToUpload(accountPage.userAwatarButton,[
+      '../../data/images/awatar.jpg'
+   ])
+   await t.click(accountPage.uploadNowButton);
+   await t.expect(accountPage.awatarMsg.withText('Upload in progress. Refresh in a few moments to see your new avatar.')).ok();
+   await t.eval(() => location.reload(true));
+   await t.setNativeDialogHandler(() => true);
+   await t.click(accountPage.deleteAwatarButton);
+   await t.expect(accountPage.awatarMsg.withText('Avatar deleted')).ok();
+});*/
+
+/*
+   test('should edit work availability', async t => {
+   
+      let salaryProfile = accountPage.salaryProfile;
+      let startingProfile = accountPage.profileStarting;
+      let startingOption = startingProfile.find('option');
+      let salaryOption = salaryProfile.find('option');
+      let isChecked  = await t.expect((accountPage.workAvailabilitySection).exists).ok();
+      await t.wait(1000);
+         if(isChecked == true){
+          accountPage.uncheckOption();
+         }
+      await t.wait(5000);
+
+   await t.click(accountPage.fullTimeCheckBox);
+   await t.click(salaryProfile);
+   await t.click(salaryOption.withText('50-60k'));
+   await t.expect(salaryProfile.value).eql('50-60k');
+   await t.click(startingProfile);
+   await t.click(startingOption.withText('immediately'));
+   await t.expect(startingProfile.value).eql('immediately');
+   await t.click(accountPage.saveProfileButton);
+   await t.expect((accountPage.successMsg).exists).ok();
    
 });
 */
+/*
+test('should add new skill', async  t =>{
+   let randomSkill = faker.lorem.word().toString();
+   await t.click(accountPage.skillsInput);
+   await t.pressKey('delete');
+   await t.typeText(accountPage.skillsInput, randomSkill);
+   await t.click(accountPage.saveProfileButton);
+   await t.expect((accountPage.successMsg).exists).ok();
+});*/
 
-test('should edit job history', async t =>{
-   let randomTitle = faker.commerce.department().toString();
-   let randomCompany = faker.company.companyName().toString();
-   accountPage.clearInput(accountPage.editJobHistoryTitle);
-   accountPage.clearInput(accountPage.editJobHistoryComapny);
-   await t.typeText(accountPage.editJobHistoryTitle, randomTitle);
-   await t.typeText(accountPage.editJobHistoryCompany, randomCompany);
-});
---
+++ tests/fixtures/jobSearch.js
@@ -0,0 +1,54 @@
+import jobBoardPage from '../../pageObjects/jobBoardPage'
+import { Selector } from 'testcafe';
+
+
+fixture('Job page')
+.beforeEach(async t => {
+   await jobBoardPage.goto();
+});
+
+
+test('should contain header', async t => {
+      await t.maximizeWindow();
+      await t.expect(jobBoardPage.header.innerText).contains("Design Jobs");
+ });
+
+ test('should contain post a job link', async t => {
+    await t.expect(jobBoardPage.postJob.innerText).contains("Post a Job");
+    await t.expect(jobBoardPage.postJob.getAttribute("href")).contains("/jobs/new");
+    await t.click(jobBoardPage.postJob);
+    await t.expect(jobBoardPage.getPageUrl()).contains('https://dribbble.com/jobs/new');
+ });
+
+ test('search for full time product designer', async t =>
+ {
+    const  checkFirstOffer = await jobBoardPage.getInnerTextOfFirstElement();
+    await jobBoardPage.typeKeyword(jobBoardPage.keywordText);
+    await jobBoardPage.selectSpecialty(jobBoardPage.container);
+    await jobBoardPage.typelocation(jobBoardPage.locationText);
+    await jobBoardPage.selectFullTimeButton();
+    await jobBoardPage.clickSearch();
+    const checkFirstOffferAfterSearch = await jobBoardPage.getInnerTextOfFirstElement();
+    await t.expect(jobBoardPage.firstOffer.withText(checkFirstOffer)).notEql((checkFirstOffferAfterSearch));
+ });
+
+ test('check if offer click redirects you to right page', async t =>
+ {
+    const  checkFirstOffer = await jobBoardPage.getInnerTextOfFirstElement();
+    await t.click(jobBoardPage.firstOffer)
+    await t.expect(jobBoardPage.firstOfferHeader.innerText).eql(checkFirstOffer);
+ });
+
+ test('verify if learn more exists and is clickable', async t =>
+ {   
+    await t.expect(jobBoardPage.learnMoreButton.innerText).eql('Learn More');
+    await t.click(jobBoardPage.learnMoreButton);
+    await t.expect(jobBoardPage.getHiredButton.innerText).eql('Get Hired');
+ });
+
+ test('verify if get pro business exists and is clickable', async t =>
+ {   
+    await t.expect(jobBoardPage.getProBusinessButton.innerText).eql('Get Pro Business');
+    await t.click(jobBoardPage.getProBusinessButton);
+    await t.expect(jobBoardPage.readyForProButton.innerText).eql('I’m ready to go Pro');
+ });
